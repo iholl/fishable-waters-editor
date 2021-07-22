ba910f5386151941953b4588d2cf11fa704ff693
iholl <ian.michael.holl@gmail.com>
2021-07-22 11:46:35 -0700

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Map />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Map
   }
 }
 </script>
@@ -21,6 +20,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+html,body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
 }
 </style>
---
+++ src/components/Map.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="mapdiv"></div>
+</template>
+
+<script>
+import MapView from '@arcgis/core/views/MapView'
+import Map from "@arcgis/core/Map"
+
+export default {
+  name: 'Map',
+  async mounted () {
+    const map = new Map ({
+      basemap: "topo-vector"
+    })
+    
+    const view = new MapView ({
+      container: this.$el,
+      map: map,
+      zoom: 6,
+      center: [-117, 38.5]
+    })
+
+    view.constraints = {
+      maxZoom: 17
+    }
+  }
+}
+</script>
+
+<style>
+@import 'https://js.arcgis.com/4.19/@arcgis/core/assets/esri/themes/dark/main.css';
+.mapdiv {
+  height: 100vh;
+}
+</style>
